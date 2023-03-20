7b42f7008c55be36aad1cfbb03faf744cdc51fb5
Baris AKIN <barisakin@gmail.com>
2023-03-21 01:36:47 +0200

    working primitive

--- src/angular.client/src/app/app.component.ts
+++ src/angular.client/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as signalR from "@microsoft/signalr"
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+
+  private hubConnection!: signalR.HubConnection;
+  input! :string;
+
+  ngOnInit() {
+    this.startConnection();
+    this.addTransferChartDataListener();
+    
+  }
+  
+  public startConnection = () => {
+    this.hubConnection = new signalR.HubConnectionBuilder()
+                            .withUrl('https://localhost:7093/chat')
+                            .build();
+    this.hubConnection
+      .start()
+      .then(() => console.log('Connection started'))
+      .catch(err => console.log('Error while starting connection: ' + err))
+  }
+
+  send() {
+    this.hubConnection.send("SendMessageToCaller", this.input);
+  }
+  
+  public addTransferChartDataListener = () => {
+    this.hubConnection.on('ReceiveMessage', (data) => {
+     
+      console.log(data);
+    });
+  }
 }
---
+++ src/net.core.api/net.core.api/ChatHub.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.DependencyInjection;
+using OpenAI.GPT3.Interfaces;
+using OpenAI.GPT3.Managers;
+using OpenAI.GPT3.ObjectModels.RequestModels;
+using OpenAI.GPT3.ObjectModels;
+using System.Text.RegularExpressions;
+
+namespace net.core.api
+{
+    public class ChatHub : Hub
+    {
+        protected IOpenAIService openAIService;
+        public ChatHub(IOpenAIService openAIService)
+        {
+            this.openAIService = openAIService;
+        }
+        public async Task SendMessageToCaller(string message)
+        {
+            var completionResult = this.openAIService.Completions.CreateCompletionAsStream(new CompletionCreateRequest()
+            {
+                Prompt = message,
+                MaxTokens = 2000
+            }, Models.TextDavinciV3);
+            
+            await foreach (var completion in completionResult)
+            {
+                if (completion.Successful)
+                {
+                    var resp = completion.Choices.FirstOrDefault()?.Text;
+                    
+                    await Clients.Caller.SendAsync("ReceiveMessage", resp);
+
+                }
+            }            
+        }
+    }
+
+    class MessagePayload
+    {
+        public string? Message { get; set; }
+        public bool Error { get; set; }
+       
+    }
+}
