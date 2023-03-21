9e7ee2ecd363f343513166014d77fdca012145db
Baris AKIN <barisakin@gmail.com>
2023-03-21 02:23:28 +0200

    funcs

--- src/net.core.api/net.core.api/ChatHub.cs
+++ src/net.core.api/net.core.api/ChatHub.cs
@@ -4,7 +4,8 @@ using OpenAI.GPT3.Interfaces;
 using OpenAI.GPT3.Managers;
 using OpenAI.GPT3.ObjectModels.RequestModels;
 using OpenAI.GPT3.ObjectModels;
-using System.Text.RegularExpressions;
+using System.Text.Json;
+
 
 namespace net.core.api
 {
@@ -15,24 +16,47 @@ namespace net.core.api
         {
             this.openAIService = openAIService;
         }
+
+        private async Task SendMessage(string message,bool error)
+        {
+            await Clients.Caller.SendAsync("ReceiveMessage", JsonSerializer.Serialize(new MessagePayload()
+            {
+                Error = error,
+                Message = message
+            }));
+        }
+       
         public async Task SendMessageToCaller(string message)
         {
-            var completionResult = this.openAIService.Completions.CreateCompletionAsStream(new CompletionCreateRequest()
+            try
             {
-                Prompt = message,
-                MaxTokens = 2000
-            }, Models.TextDavinciV3);
-            
-            await foreach (var completion in completionResult)
-            {
-                if (completion.Successful)
+                var completionResult = this.openAIService.Completions.CreateCompletionAsStream(new CompletionCreateRequest()
                 {
-                    var resp = completion.Choices.FirstOrDefault()?.Text;
-                    
-                    await Clients.Caller.SendAsync("ReceiveMessage", resp);
+                    Prompt = message,
+                    MaxTokens = 2000
+                }, Models.TextDavinciV3);
 
+                await foreach (var completion in completionResult)
+                {
+                    if (completion.Successful)
+                    {
+                        var resp = completion.Choices.FirstOrDefault()?.Text;
+                        await SendMessage(resp, false);                       
+                    }
+                    else
+                    {
+                        if (completion.Error == null)
+                        {
+                            await SendMessage("Unknown Error, ending stream", true);
+                            throw new Exception("Unknown Error");
+                        }
+                        await SendMessage($"{completion.Error.Code}: {completion.Error.Message}", true);                                         
+                    }
                 }
-            }            
+            } catch (Exception ex)
+            {
+                await SendMessage(ex.Message, true);            
+            }
         }
     }
 
